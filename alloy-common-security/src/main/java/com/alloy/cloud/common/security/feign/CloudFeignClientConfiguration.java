e606790874bbee233fb74e1e389d7b9afe982c01
tankechao <tankechao@quanyou.com>
2020-12-07 13:52:23 +0800

    [FIX]add base class

--- alloy-common-security/src/main/java/com/alloy/cloud/common/security/feign/CloudFeignClientConfiguration.java
+++ alloy-common-security/src/main/java/com/alloy/cloud/common/security/feign/CloudFeignClientConfiguration.java
@@ -40,4 +40,8 @@ public class CloudFeignClientConfiguration {
 		return new CloudFeignClientInterceptor(oAuth2ClientContext, resource, accessTokenContextRelay);
 	}
 
+//	@Bean
+//	public RequestInterceptor getUserFeignClientInterceptor() {
+//		return new UserFeignClientInterceptor();
+//	}
 }
