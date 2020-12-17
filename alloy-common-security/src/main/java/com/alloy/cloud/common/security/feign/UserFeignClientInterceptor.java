30222c9eeee368f59e5531af4dbc4e8faaa5e08c
tankechao <tankechao@quanyou.com>
2020-12-17 11:30:38 +0800

    [FIX]Feign配置修改

--- alloy-common-security/src/main/java/com/alloy/cloud/common/security/feign/UserFeignClientInterceptor.java
+++ alloy-common-security/src/main/java/com/alloy/cloud/common/security/feign/UserFeignClientInterceptor.java
@@ -5,7 +5,6 @@ import com.alloy.cloud.common.core.constant.SecurityConstants;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
